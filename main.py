96ab8ba166c32fdf0287245a093ebc1a08bbbc5c
Fish <zhenyu2004@hotmail.com>
2016-06-12 09:21:33 +0800

    added base.html template

--- main.py
+++ main.py
@@ -26,6 +26,21 @@ class Handler(webapp2.RequestHandler):
     def render(self,template,**kw):
         self.write(self.render_str(template,**kw))
 
+class EditHandler(Handler):
+    def get(self):
+        self.render("edit.html")
+        
+    def post(self):
+        title = self.request.get("title")
+        content = self.request.get("content")
+        if title and content:
+            post =Post(title=title, content=content)
+            post.put()
+            self.redirect("/")
+        else:
+            error ="Both title and content are requried!! Please check."
+            self.render("edit.html",title =title,content=content,error=error)
+
 class PostHandler(Handler):
     def get(self,post_id):#the mapping url id will go here
         post_id= int(post_id)
@@ -45,19 +60,12 @@ class MainHandler(Handler):
         for post in posts:
             logging.info(post.key().id())
         self.render("index.html",title=title,content=content,error=error,posts = posts)   
+
     def get(self):
         self.render_front()
-    def post(self):
-        title = self.request.get("title")
-        content = self.request.get("content")
-        if title and content:
-            post =Post(title=title, content=content)
-            post.put()
-            self.redirect("/")
-        else:
-            error ="Both title and content are requried!! Please check."
-            self.render_front(title =title,content=content,error=error)
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),(r'/(\d+)',PostHandler)
+   ('/edit',EditHandler),   ('/', MainHandler),(r'/(\d+)',PostHandler)
 ], debug=True)
